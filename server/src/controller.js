95ce720b63b23e64cdf802de680a4d3f4ef61046
Ryan Hiller <ryanhillerdev@gmail.com>
2021-12-15 15:01:34 -0700

    Documentation changes

--- server/src/controller.js
+++ server/src/controller.js
@@ -1,11 +1,13 @@
 const Task = require('./model')
 const asyncWrapper = require('./middleware/async')
 
+// Query database for all tasks
 const getAllTasks = asyncWrapper(async (req, res) => {
   const tasks = await Task.find({})
   res.status(200).json({ tasks })
 })
 
+// Query database for individual task
 const getTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params
   const task = await Task.findOne({ _id: taskID })
@@ -17,11 +19,13 @@ const getTask = asyncWrapper(async (req, res) => {
   res.status(200).json({ task })
 })
 
+// Create a task in the database
 const createTask = asyncWrapper(async (req, res) => {
   const task = await Task.create(req.body)
   res.status(201).json({ task })
 })
 
+// Modify an existing task in the database
 const updateTask = asyncWrapper(async (req, res) => {
   const { id: taskID } = req.params
   const task = await Task.findOneAndUpdate({ _id: taskID }, req.body, {
